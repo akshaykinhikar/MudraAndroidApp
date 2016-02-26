4444de69257168249552ba4e33052f556bed9a86
akshay <kinhikar.akshay32@gmail.com>
2016-02-26 13:57:42 +0530

    UI-- login activity

--- Mudra/app/src/main/java/com/example/akshay/mudra/LoginActivity.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/LoginActivity.java
@@ -2,6 +2,10 @@ package com.example.akshay.mudra;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText userName = (EditText) findViewById(R.id.et_username);
+        EditText password = (EditText) findViewById(R.id.et_password);
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("msg", "login Button CLicked");
+                userName.getText().toString();
+            }
+        });
+
     }
 }
