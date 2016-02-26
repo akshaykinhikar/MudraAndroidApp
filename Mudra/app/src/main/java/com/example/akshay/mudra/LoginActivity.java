628af277fe0142794647a09817a95071f7b8239f
akshay <kinhikar.akshay32@gmail.com>
2016-02-27 04:10:17 +0530

    Post Request -- Login Page

--- Mudra/app/src/main/java/com/example/akshay/mudra/LoginActivity.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/LoginActivity.java
@@ -7,6 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -15,13 +26,31 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         final EditText userName = (EditText) findViewById(R.id.et_username);
-        EditText password = (EditText) findViewById(R.id.et_password);
+        final EditText passwd = (EditText) findViewById(R.id.et_password);
         Button btn_login = (Button) findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("msg", "login Button CLicked");
-                userName.getText().toString();
+                Log.d("msg", "login Button Clicked");
+                String username = userName.getText().toString();
+                String password = passwd.getText().toString();
+
+                JSONObject jsonobj = new JSONObject();
+                try {
+                    jsonobj.put("username", username);
+                    jsonobj.put("password", password);
+
+                    AsyncHttpClient login = new AsyncHttpClient();
+                    try {
+                        login.post(LoginActivity.this, "http://192.168.1.125:8000/user_login/", new StringEntity(jsonobj.toString()),"application/json", new JsonHttpResponseHandler(
+                        ));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
