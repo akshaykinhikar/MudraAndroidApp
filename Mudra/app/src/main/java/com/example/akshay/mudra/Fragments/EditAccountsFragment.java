6e307a35f618207d862844bfdb416e3a0c87a922
akshay <kinhikar.akshay32@gmail.com>
2016-03-01 04:02:40 +0530

    Intermediate commit

--- Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/EditAccountsFragment.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/EditAccountsFragment.java
@@ -52,7 +52,6 @@ public class EditAccountsFragment extends Fragment {
         mListener = null;
     }
 
-
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
