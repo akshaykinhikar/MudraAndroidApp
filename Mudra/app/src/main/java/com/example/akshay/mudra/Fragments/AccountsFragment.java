5ac8c631d6c9fde370e8a1e66d87b7220dd64cca
akshay <kinhikar.akshay32@gmail.com>
2016-02-29 16:13:41 +0530

    UI-- listview for Financial Year

--- Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/AccountsFragment.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/AccountsFragment.java
@@ -4,68 +4,64 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.example.akshay.mudra.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  * {@link AccountsFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link AccountsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AccountsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class AccountsFragment extends ListFragment {
+    ListView listView;
     private OnFragmentInteractionListener mListener;
+    String[] years = {"1","2","3","4"};
+    String[] years1 = {"1","2","3","4"};
+    ArrayList<HashMap <String, String>> data = new ArrayList<HashMap<String, String>>();
+    SimpleAdapter adapter;
 
     public AccountsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AccountsFragment newInstance(String param1, String param2) {
-        AccountsFragment fragment = new AccountsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+       HashMap<String, String> map = new HashMap<String, String>();
+
+        for(int i = 0; i < years.length; i++ ){
+            map = new HashMap<String, String>();
+            map.put("years",years[i]);
+            map.put("years1",years1[i]);
+            data.add(map);
+        }
+
+        //keys in map
+        String[] from = {"years", "years1"};
+
+        int[] to = {R.id.years, R.id.years1};
+
+        adapter = new SimpleAdapter(getActivity(), data, R.layout.model, from, to);
+        setListAdapter(adapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_accounts, container, false);
+        View v =  inflater.inflate(R.layout.fragment_accounts, container, false);
+
+        return v;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -87,22 +83,25 @@ public class AccountsFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+
+        super.onStart();
+
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), data.get(position).get("years") , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
+        public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
