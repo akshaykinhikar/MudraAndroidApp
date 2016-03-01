d9d28c4d9a5bdd0087db05c17367f56f65251425
akshay <kinhikar.akshay32@gmail.com>
2016-03-01 13:29:44 +0530

    (00) User Registered

--- Mudra/app/src/main/java/com/example/akshay/mudra/RegistrationActivity.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/RegistrationActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.akshay.mudra.Utility.Utility;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -15,6 +16,7 @@ import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 
+import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -30,27 +32,46 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d("msg", "register Button Clicked");
 
-                JSONObject userInfo = new JSONObject();
+                JSONObject userRegisterDetail = new JSONObject();
                 try {
-                    userInfo.put("userName", ((EditText) findViewById(R.id.et_username)).getText().toString());
-                    userInfo.put("password", ((EditText) findViewById(R.id.et_password)).getText().toString());
-                    userInfo.put("confirmPassword",((EditText) findViewById(R.id.et_password1)).getText().toString());
-                    userInfo.put("firstName",((EditText) findViewById(R.id.et_fname)).getText().toString());
-                    userInfo.put("lastName",((EditText) findViewById(R.id.et_lname)).getText().toString());
-                    userInfo.put("addressLine1",((EditText) findViewById(R.id.et_add_line_1)).getText().toString());
-                    userInfo.put("addressLine2",((EditText) findViewById(R.id.et_add_line_2)).getText().toString());
-                    userInfo.put("city",((EditText) findViewById(R.id.et_city)).getText().toString());
-                    userInfo.put("state",((EditText) findViewById(R.id.et_state)).getText().toString());
-                    userInfo.put("pincode",((EditText) findViewById(R.id.et_pin)).getText());
-                    userInfo.put("email",((EditText) findViewById(R.id.et_email)).getText().toString());
-                    userInfo.put("country",((EditText) findViewById(R.id.et_country)).getText().toString());
-                    userInfo.put("mobileNo0",((EditText) findViewById(R.id.et_mobileNo)).getText());
-                    userInfo.put("mobileNo1",((EditText) findViewById(R.id.et_alternateMobileNo)).getText());
+                    userRegisterDetail.put("userName", ((EditText) findViewById(R.id.et_username)).getText().toString());
+                    userRegisterDetail.put("password", ((EditText) findViewById(R.id.et_password)).getText().toString());
+                    userRegisterDetail.put("confirmPassword",((EditText) findViewById(R.id.et_password1)).getText().toString());
+                    userRegisterDetail.put("firstName",((EditText) findViewById(R.id.et_fname)).getText().toString());
+                    userRegisterDetail.put("lastName",((EditText) findViewById(R.id.et_lname)).getText().toString());
+                    userRegisterDetail.put("addressLine1",((EditText) findViewById(R.id.et_add_line_1)).getText().toString());
+                    userRegisterDetail.put("addressLine2",((EditText) findViewById(R.id.et_add_line_2)).getText().toString());
+                    userRegisterDetail.put("city",((EditText) findViewById(R.id.et_city)).getText().toString());
+                    userRegisterDetail.put("state",((EditText) findViewById(R.id.et_state)).getText().toString());
+                    userRegisterDetail.put("pincode",((EditText) findViewById(R.id.et_pin)).getText());
+                    userRegisterDetail.put("email",((EditText) findViewById(R.id.et_email)).getText().toString());
+                    userRegisterDetail.put("country",((EditText) findViewById(R.id.et_country)).getText().toString());
+                    userRegisterDetail.put("mobileNo0",((EditText) findViewById(R.id.et_mobileNo)).getText());
+                    userRegisterDetail.put("mobileNo1",((EditText) findViewById(R.id.et_alternateMobileNo)).getText());
 
                     AsyncHttpClient registerUser = new AsyncHttpClient();
                     try {
-                        registerUser.post(RegistrationActivity.this, "http://192.168.1.125:8000/register_new_user/", new StringEntity(userInfo.toString()),"application/json", new JsonHttpResponseHandler(
-                        ));
+                        JSONObject userInfo = new JSONObject();
+                        userInfo.put("userInfo", userRegisterDetail);
+                        if(Utility.isNetConnected(getApplicationContext())){
+                            registerUser.post(RegistrationActivity.this, "http://192.168.1.125:8000/register_new_user/",
+                                    new StringEntity(userInfo.toString()),"application/json",
+                                    new JsonHttpResponseHandler(){
+                                        @Override
+                                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                                            super.onSuccess(statusCode, headers, response);
+                                            Log.d("msg", "response" + response);
+                                        }
+
+                                        @Override
+                                        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                                            super.onFailure(statusCode, headers, responseString, throwable);
+                                            Log.d("msg", "error Response" +responseString);
+                                        }
+                                    });
+                        }
+
+
                     } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
                     }
