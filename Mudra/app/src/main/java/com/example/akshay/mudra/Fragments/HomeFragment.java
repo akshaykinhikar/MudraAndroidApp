20a15759c2e88ce645212fd5ec04268ea58039a9
akshay <kinhikar.akshay32@gmail.com>
2016-03-01 15:19:33 +0530

    Accounting years list UI created

--- Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/HomeFragment.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/HomeFragment.java
@@ -4,11 +4,19 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.example.akshay.mudra.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,7 +25,14 @@ import com.example.akshay.mudra.R;
  * to handle interaction events.
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends ListFragment {
+
+    ListView listView;
+    String[] accStartYear = {"1","2", "3","4"};
+    String[] accEndYear = {"1","2", "3","4"};
+    ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+    SimpleAdapter adapter;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,8 +49,32 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        HashMap<String, String> map =  new HashMap<String,String>();
+        for(int i =0; i<accStartYear.length; i++){
+            map = new HashMap<String, String>();
+            map.put("startYear",accStartYear[i]);
+            map.put("endYear",accEndYear[i]);
+            data.add(map);
+        }
+
+        String[] from = {"startYear", "endYear"};
+        int[] to = {R.id.start_date, R.id.end_date};
+        adapter = new SimpleAdapter(getActivity(), data, R.layout.accounting_year_item, from, to);
+        setListAdapter(adapter);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), data.get(position).get("startYear"), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
