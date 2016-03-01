654e522c07b520fc8acda98b37f0778da1c35156
akshay <kinhikar.akshay32@gmail.com>
2016-03-01 12:37:24 +0530

    Registration-- intermediate commit

--- Mudra/app/src/main/java/com/example/akshay/mudra/RegistrationActivity.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/RegistrationActivity.java
@@ -30,43 +30,26 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d("msg", "register Button Clicked");
 
-
-//                String userName = ;
-//                String password = ((EditText) findViewById(R.id.et_password)).getText().toString();
-//                String confirmPassword = ((EditText) findViewById(R.id.et_password1)).getText().toString();
-//                String firstName = ((EditText) findViewById(R.id.et_fname)).getText().toString();
-//                String lastName = ((EditText) findViewById(R.id.et_lname)).getText().toString();
-//                String addressLine1 = ((EditText) findViewById(R.id.et_add_line_1)).getText().toString();
-//                String addressLine2 = ((EditText) findViewById(R.id.et_add_line_2)).getText().toString();
-//                String city = ((EditText) findViewById(R.id.et_city)).getText().toString();
-//                String state = ((EditText) findViewById(R.id.et_state)).getText().toString();
-//                String pincode = ((EditText) findViewById(R.id.et_pin)).getText().toString();
-//                String email = ((EditText) findViewById(R.id.et_email)).getText().toString();
-//                String country = ((EditText) findViewById(R.id.et_country)).getText().toString();
-//                String mobileNo0 = ((EditText) findViewById(R.id.et_mobileNo)).getText().toString();
-//                String mobileNo1 = ((EditText) findViewById(R.id.et_alternateMobileNo)).getText().toString();
-//                Log.d("msg", "register Button Clicked " + userName);
-
-                JSONObject registrationobj = new JSONObject();
+                JSONObject userInfo = new JSONObject();
                 try {
-                    registrationobj.put("userName", ((EditText) findViewById(R.id.et_username)).getText().toString());
-                    registrationobj.put("password", ((EditText) findViewById(R.id.et_password)).getText().toString());
-                    registrationobj.put("confirmPassword",((EditText) findViewById(R.id.et_password1)).getText().toString());
-                    registrationobj.put("firstName",((EditText) findViewById(R.id.et_fname)).getText().toString());
-                    registrationobj.put("lastName",((EditText) findViewById(R.id.et_lname)).getText().toString());
-                    registrationobj.put("addressLine1",((EditText) findViewById(R.id.et_add_line_1)).getText().toString());
-                    registrationobj.put("addressLine2",((EditText) findViewById(R.id.et_add_line_2)).getText().toString());
-                    registrationobj.put("city",((EditText) findViewById(R.id.et_city)).getText().toString());
-                    registrationobj.put("state",((EditText) findViewById(R.id.et_state)).getText().toString());
-                    registrationobj.put("pincode",((EditText) findViewById(R.id.et_pin)).getText().toString());
-                    registrationobj.put("email",((EditText) findViewById(R.id.et_email)).getText().toString());
-                    registrationobj.put("country",((EditText) findViewById(R.id.et_country)).getText().toString());
-                    registrationobj.put("mobileNo0",((EditText) findViewById(R.id.et_mobileNo)).getText().toString());
-                    registrationobj.put("mobileNo1",((EditText) findViewById(R.id.et_alternateMobileNo)).getText().toString());
+                    userInfo.put("userName", ((EditText) findViewById(R.id.et_username)).getText().toString());
+                    userInfo.put("password", ((EditText) findViewById(R.id.et_password)).getText().toString());
+                    userInfo.put("confirmPassword",((EditText) findViewById(R.id.et_password1)).getText().toString());
+                    userInfo.put("firstName",((EditText) findViewById(R.id.et_fname)).getText().toString());
+                    userInfo.put("lastName",((EditText) findViewById(R.id.et_lname)).getText().toString());
+                    userInfo.put("addressLine1",((EditText) findViewById(R.id.et_add_line_1)).getText().toString());
+                    userInfo.put("addressLine2",((EditText) findViewById(R.id.et_add_line_2)).getText().toString());
+                    userInfo.put("city",((EditText) findViewById(R.id.et_city)).getText().toString());
+                    userInfo.put("state",((EditText) findViewById(R.id.et_state)).getText().toString());
+                    userInfo.put("pincode",((EditText) findViewById(R.id.et_pin)).getText());
+                    userInfo.put("email",((EditText) findViewById(R.id.et_email)).getText().toString());
+                    userInfo.put("country",((EditText) findViewById(R.id.et_country)).getText().toString());
+                    userInfo.put("mobileNo0",((EditText) findViewById(R.id.et_mobileNo)).getText());
+                    userInfo.put("mobileNo1",((EditText) findViewById(R.id.et_alternateMobileNo)).getText());
 
                     AsyncHttpClient registerUser = new AsyncHttpClient();
                     try {
-                        registerUser.post(RegistrationActivity.this, "http://192.168.1.125:8000/user_login/", new StringEntity(registrationobj.toString()),"application/json", new JsonHttpResponseHandler(
+                        registerUser.post(RegistrationActivity.this, "http://192.168.1.125:8000/register_new_user/", new StringEntity(userInfo.toString()),"application/json", new JsonHttpResponseHandler(
                         ));
                     } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
