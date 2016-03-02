65534f45c146315edddf046aea766bba65d54805
akshay <kinhikar.akshay32@gmail.com>
2016-03-02 17:12:20 +0530

    (00)Accounts Detail showed

--- Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/AccountsFragment.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/AccountsFragment.java
@@ -16,9 +16,19 @@ import android.widget.Toast;
 
 import com.example.akshay.mudra.HomeActivity;
 import com.example.akshay.mudra.R;
+import com.example.akshay.mudra.Utility.Utility;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,8 +40,9 @@ import java.util.HashMap;
 public class AccountsFragment extends ListFragment {
     ListView listView;
     private OnFragmentInteractionListener mListener;
-    String[] years = {"1","2","3","4"};
-    String[] years1 = {"1","2","3","4"};
+    List<String> account_name = new ArrayList<>();
+    List<String> account_amount = new ArrayList<>();
+
     ArrayList<HashMap <String, String>> data = new ArrayList<HashMap<String, String>>();
     SimpleAdapter adapter;
 
@@ -42,28 +53,68 @@ public class AccountsFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-       HashMap<String, String> map = new HashMap<String, String>();
 
-        for(int i = 0; i < years.length; i++ ){
-            map = new HashMap<String, String>();
-            map.put("years",years[i]);
-            map.put("years1",years1[i]);
-            data.add(map);
+        AsyncHttpClient accountsReq = new AsyncHttpClient();
+        PersistentCookieStore accounts_cookies = new PersistentCookieStore(getContext());
+        accountsReq.setCookieStore(accounts_cookies);
+        try {
+            if (Utility.isNetConnected(getContext())) {
+                accountsReq.get("http://192.168.1.125:8000/show_account_details/", new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        super.onSuccess(statusCode, headers, response);
+                        Log.d("acc_fra", "accountsReq Res " + response);
+                        try {
+                            int arrayLength = response.getJSONArray("accountList").length();
+                            if (arrayLength != 0) {
+                                for (int i = 0; i < arrayLength; i++) {
+                                    account_name.add(response.getJSONArray("accountList").getJSONObject(i).getString("account_name"));
+                                    account_amount.add(response.getJSONArray("accountList").getJSONObject(i).getString("amount"));
+                                    Log.d("acc_fra", "" + account_name + account_amount);
+                                    setAccountsDetail();
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        super.onFailure(statusCode, headers, responseString, throwable);
+                        Log.d("acc_fra", "accountReq ErrRes");
+                    }
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        //keys in map
-        String[] from = {"years", "years1"};
-
-        int[] to = {R.id.years, R.id.years1};
-
-        adapter = new SimpleAdapter(getActivity(), data, R.layout.model, from, to);
-        setListAdapter(adapter);
 
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_accounts, container, false);
 
         return v;
 
+    }
+
+
+    public void setAccountsDetail(){
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        for(int i = 0; i < account_name.size(); i++ ){
+            map = new HashMap<String, String>();
+            map.put("acc_name",account_name.get(i));
+            map.put("acc_amount",account_amount.get(i));
+            data.add(map);
+        }
+
+        //keys in map
+        String[] from = {"acc_name", "acc_amount"};
+
+        int[] to = {R.id.years, R.id.years1};
+
+        adapter = new SimpleAdapter(getActivity(), data, R.layout.model, from, to);
+        setListAdapter(adapter);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/HomeFragment.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/HomeFragment.java
@@ -107,7 +107,7 @@ public class HomeFragment extends ListFragment {
         } catch (Exception e) {
             e.printStackTrace();
         }
- 
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
