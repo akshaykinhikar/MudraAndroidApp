ddd83f2ee5353bd1f1a063d2f052aac32d5ccd5e
akshay <kinhikar.akshay32@gmail.com>
2016-03-02 18:37:28 +0530

    (00)Edit Account

--- Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/EditAccountsFragment.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/EditAccountsFragment.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +15,25 @@ import android.widget.Toast;
 
 import com.example.akshay.mudra.HomeActivity;
 import com.example.akshay.mudra.R;
+import com.example.akshay.mudra.Utility.Utility;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 public class EditAccountsFragment extends ListFragment {
 
     private OnFragmentInteractionListener mListener;
-    String[] Acc = {"a","b","c","d"};
-    String[] Amount = {"A", "B", "C", "D"};
+    List<String> account_name = new ArrayList<>();
+    List<String> account_amount = new ArrayList<>();
 
     ArrayList<HashMap<String,String>> data = new ArrayList<HashMap<String, String>>();
     SimpleAdapter adapter;
@@ -34,25 +45,62 @@ public class EditAccountsFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        HashMap<String, String> map = new HashMap<String, String>();
-        for(int i=0; i<Acc.length; i++){
-            map = new HashMap<String, String>();
-            map.put("acc", Acc[i]);
-            map.put("amount", Amount[i]);
-            data.add(map);
-        }
-
-        String[] from = {"acc", "amount"};
-        int[] to ={R.id.acc_name, R.id.acc_amount};
-        adapter = new SimpleAdapter(getActivity(), data, R.layout.account_list_for_edit, from, to);
-        setListAdapter(adapter);
+//
+//
+//
+//
+//        HashMap<String, String> map = new HashMap<String, String>();
+//        for(int i=0; i<Acc.length; i++){
+//            map = new HashMap<String, String>();
+//            map.put("acc", Acc[i]);
+//            map.put("amount", Amount[i]);
+//            data.add(map);
+//        }
+//
+//        String[] from = {"acc", "amount"};
+//        int[] to ={R.id.acc_name, R.id.acc_amount};
+//        adapter = new SimpleAdapter(getActivity(), data, R.layout.account_list_for_edit, from, to);
+//        setListAdapter(adapter);
 
       }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        AsyncHttpClient accountsReq = new AsyncHttpClient();
+        PersistentCookieStore account_cookies = new PersistentCookieStore(getContext());
+        accountsReq.setCookieStore(account_cookies);
+
+        try {
+            if(Utility.isNetConnected(getContext())){
+                accountsReq.get("http://192.168.1.125:8000/show_account_details/", new JsonHttpResponseHandler(){
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        super.onSuccess(statusCode, headers, response);
+
+                        try {
+                            int arrayLength = response.getJSONArray("accountList").length();
+                            for(int i = 0; i < arrayLength; i++){
+                                account_name.add(response.getJSONArray("accountList").getJSONObject(i).getString("account_name"));
+                                account_amount.add(response.getJSONArray("accountList").getJSONObject(i).getString("amount"));
+                                Log.d("edit_acc_fra", "" + account_name + account_amount);
+                            }
+                            setAccountsDetail();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        super.onFailure(statusCode, headers, responseString, throwable);
+                        Log.d("msg","responseString" +responseString);
+                    }
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit_accounts, container, false);
     }
@@ -74,14 +122,23 @@ public class EditAccountsFragment extends ListFragment {
         void editAccountFragmentInterface();
     }
 
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void setAccountsDetail(){
+        HashMap<String, String> map = new HashMap<String, String>();
+        for(int i = 0; i < account_name.size(); i++ ){
+            map = new HashMap<String, String>();
+            map.put("acc_name",account_name.get(i));
+            map.put("acc_amount",account_amount.get(i));
+            data.add(map);
+        }
+        //keys in map
+        String[] from = {"acc_name", "acc_amount"};
+        int[] to = {R.id.acc_name, R.id.acc_amount};
+        adapter = new SimpleAdapter(getActivity(), data, R.layout.account_list_for_edit, from, to);
+        setListAdapter(adapter);
     }
 
-    public interface OnFragmentInteractionListener {
+
+     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
