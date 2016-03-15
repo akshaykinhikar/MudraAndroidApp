ea0ce31ee6f2190ae87e31bb0c52eb8342de5a37
akshay <kinhikar.akshay32@gmail.com>
2016-03-15 12:38:37 +0530

    UI-- intermediate commit

--- Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/EventAccountingFragment.java
+++ Mudra/app/src/main/java/com/example/akshay/mudra/Fragments/EventAccountingFragment.java
@@ -8,8 +8,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.akshay.mudra.LoginActivity;
 import com.example.akshay.mudra.R;
@@ -28,12 +30,13 @@ import cz.msebera.android.httpclient.Header;
 
 public class EventAccountingFragment extends Fragment {
 
-    Spinner spinner_transaction_mode;
+    Spinner spinner_transaction_mode,spinner_account_action_credit_debit;
 
     private OnFragmentInteractionListener mListener;
 
     // Spinner Drop down elements
     List<String> transaction_modeList = new ArrayList<String>();
+    List<String> action_credit_debit =  new ArrayList<>();
 
     public EventAccountingFragment() {
         // Required empty public constructor
@@ -47,6 +50,7 @@ public class EventAccountingFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_event_accounting, container, false);
 
         spinner_transaction_mode = (Spinner) view.findViewById(R.id.spinner_transaction_mode);
+        spinner_account_action_credit_debit = (Spinner) view.findViewById(R.id.spinner_credit_debit);
 
         Log.d("eventAcc"," spinner to show" +transaction_modeList);
 
@@ -95,6 +99,28 @@ public class EventAccountingFragment extends Fragment {
 //        ++++++++++++++++++++++++++++++++++++++++++++++++
 //        ++++++++++++++++++++++++++++++++++++++++++++++++
 //        ++++++++++++++++++++++++++++++++++++++++++++++++
+//        listener for spinner_transaction_mode
+        spinner_transaction_mode.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Toast.makeText(getActivity(), "" + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //spinner credit or Debit
+        action_credit_debit.add("Credit");
+        action_credit_debit.add("debit");
+
+        ArrayAdapter<String> adapter_credit_debit = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item, action_credit_debit);
+
+        adapter_credit_debit.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner_account_action_credit_debit.setAdapter(adapter_credit_debit);
+
 
 
 
